d32e5006af19ecaeae279959173893e2627617b9
0rientd <chbarros.junior@gmail.com>
2021-02-21 12:07:05 -0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by 0rientd on 21/02/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
